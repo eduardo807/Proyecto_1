b1e3a1a86572a2fe4c99860668e6e63e4048587d
eduardo807 <morilloeduardo807@gmail.com>
2018-05-14 02:52:53 -0400

    Se agrearon mas campos

---
+++ sugerencias/model/suge.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+
+from odoo import api, fields, models, _
+
+
+
+
+class sugerecla(models.Model):
+    _name = "suge.recla"
+    
+    
+
+    name = fields.Char(string='Nombre', required=True, readonly=False,)
+	
+	apell = fields.Char(string='Apellido', required=True, readonly=False,)
+
+	carr = fields.Char(string='Carrera', required=True, readonly=False,)
