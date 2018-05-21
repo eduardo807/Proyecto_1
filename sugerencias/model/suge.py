f79e470f6fb3c0fb562e12655b0217d4c11ba152
eduardo807 <morilloeduardo807@gmail.com>
2018-05-20 21:48:38 -0400

    se terminaron de agregar todos los campos y se le hizo un codigo con logica propia.

--- sugerencias/model/suge.py
+++ sugerencias/model/suge.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-
-
 from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
 
@@ -11,10 +10,26 @@ from odoo import api, fields, models, _
 class sugerecla(models.Model):
     _name = "suge.recla"
     
+    name = fields.Char(string='Nombre:', required=True, readonly=False,)
+    apell = fields.Char(string='Apellido:', required=True, readonly=False,)
+    ced = fields.Char(string='Cédula:', required=True, readonly=False, )
+    corr = fields.Char(string='Correo:', required=True, readonly=False,)
+    tlf = fields.Char(string='Teléfono:', required=True, readonly=False,)
+    sex = fields.Selection([
+        ('mas', 'Masculino'),
+        ('fem', 'Femenino'),
+        ('ind', 'Indefinido'),
+        ], string='Sexo:', compute='_compute_invoice_status', store=True, readonly=False)
+    semes = fields.Char(string='Semestre:', required=True, readonly=False,)
+    carr = fields.Char(string='Carrera:', required=True, readonly=False,)
+    suge = fields.Text(string='Sugerencias o Reclamos:', required=True, readonly=False,)
     
-
-    name = fields.Char(string='Nombre', required=True, readonly=False,)
-	
-	apell = fields.Char(string='Apellido', required=True, readonly=False,)
-
-	carr = fields.Char(string='Carrera', required=True, readonly=False,)
+    def create(self, vals):
+        Sugerencias = vals['suge']   
+        malas_palabras = ['culo','guebo','mamaguebo','maduro','mardito','trimardito']
+        lista_sugerencias = Sugerencias.split('')
+        for i in lista_sugerencias:
+			if i in malas_palabras:
+			    raise UserError(_('No se aceptan malas palabras.'))
+		 
+        return super(SaleOrderLine, self).create(vals)
